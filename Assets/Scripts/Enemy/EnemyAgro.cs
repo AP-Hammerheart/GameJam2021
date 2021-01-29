4ea43eefadcc9ae5dd0a59e7e77e985e240aeec3
Modersohn16 <40393958+Modersohn16@users.noreply.github.com>
2021-01-29 23:55:52 +0100

    Some cleanin

--- Assets/Scripts/Enemy/EnemyAgro.cs
+++ Assets/Scripts/Enemy/EnemyAgro.cs
@@ -11,7 +11,7 @@ public class EnemyAgro : MonoBehaviour
     [SerializeField]
     float moveSpeed;
 
-    Rigidbody2D rb2d;
+    public Rigidbody2D rb2d;
     
 
     // Start is called before the first frame update
@@ -45,20 +45,20 @@ public class EnemyAgro : MonoBehaviour
         if(transform.position.x < player.position.x)
         {
             //enemy is to the left side of the player, so move right
-            rd2b.velocity = new Vector2.(moveSpeed, 0);
+            rb2d.velocity = new Vector2(moveSpeed, 0);
             transform.localScale = new Vector2(1, 1);
         }
         else if (transform.position.x > player.position.x)
         {
             //enemy is to the right side of the player, so move left
-            rd2b.velocity = new Vector2.(-moveSpeed, 0);
+            rb2d.velocity = new Vector2(-moveSpeed, 0);
             transform.localScale = new Vector2(-1, 1);
         }
     }
 
     void StopChasingPlayer()
     {
-        rd2b.velocity = new Vector2(0, 0);
+        rb2d.velocity = new Vector2(0, 0);
     }
 
 }
